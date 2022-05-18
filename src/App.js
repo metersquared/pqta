02f9208c010085ee6d53b4c51555e9d6a5424a3c
Arya Prasetya <arya.prasetya1997@gmail.com>
2022-05-18 22:56:48 +0200

    Change index

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Change stuff
         </p>
         <a
           className="App-link"
