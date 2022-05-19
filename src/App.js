0676da90dbd1ef69fe0f672d5e2134f9dc6b4e86
Arya Prasetya <arya.prasetya1997@gmail.com>
2022-05-19 03:42:38 +0200

    Finishing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+
+import music from './public/autumnmelody.mp3';
+import gl from './public/good luck.jpg';
 import './App.css';
+import React, {useState} from "react";
+import Fader from './components/Fader';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: white;
+  color: black;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-color: black;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+  flex:0;
+`;
 
 function App() {
+
+  const [visibility, setVisibility] = useState(false
+  );
+
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [textIndex,setTextIndex]= useState(0);
+
+  const sentences =[
+    "Oops...",
+    "Hallo Ita,... ",
+    "I hope everything's going well...",
+    "Berhubung kamu bakal ujian (terakhir..?),",
+    "aku mau bilang...",
+    "Semangat ya 🍀",
+    "Sori ya, aku nggak bisa bantuin kali ini. 😅",
+    "Tapi aku yakin kamu pasti bisa. 😉",
+    "I have not met a women as smart, resilient and determined as you.",
+    "Lebay ya, gini doang pake bikin website.",
+    "Ya tapi gimana lagi,... ",
+    "Aku nggak sejago kamu bikin postcard. 🤷‍♂️",
+    "Gitu aja sih,... ",
+    "Doaku bersamamu Ta, ditunggu kabarnya setelah ujian.",
+    "Nanti kalo mau curhat dan ngomel2in juga boleh...",
+    "Salam dari 🐻lin,",
+    "Arya ❤️"
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Change stuff
-        </p>
-        <button>Stuff</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {buttonClicked&&<body>
+    <div  className='App fill-height'>
+    {textIndex<sentences.length&&<Fader setTextIndex={setTextIndex} textIndex={textIndex} visible={visibility} text={sentences[textIndex]}/>}
+    {textIndex>=sentences.length&&<div className='fade-in' style={{alignItems:"center",}}><img  height={166} width={709} src={gl}/></div>}
+    </div>
+    </body>}
+    
+    {!buttonClicked&& <body className='App fill-height'> <Button style={{margin:"30%"}}  onClick={()=>{setVisibility(!visibility); new Audio(music).play();setButtonClicked(true)}}>Klausur_SoSe2022_Lösung.pdf</Button></body>}
     </div>
   );
 }
---
+++ src/components/Fader.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from "react";
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: white;
+  color: black;
+  font-size: 20px;
+  padding: 10px 10px;
+  border-radius: 5px;
+  border-width: 0px;
+  margin: 10px 10px;
+  cursor: pointer;
+  flex:0;
+`;
+
+const Fader = ({text,visible, textIndex, setTextIndex}) =>{
+
+    const [fadeProp, setFadeProp] = useState({
+        fade: 'fade-in'
+    });
+
+    const [currentText, setCurrentText] = useState(text);
+    const [currentTextIndex, setCurrentTextIndex] = useState(textIndex);
+
+    useEffect(()=>{
+                
+                setTimeout(()=>{
+                    setCurrentTextIndex(textIndex);
+                    setCurrentText(text);
+                    setFadeProp({
+                        fade: 'fade-in'
+                    });
+                },800)
+                
+        
+        return()=>{}
+    },[text])
+
+    return(
+        <div className={"row "+fadeProp.fade}>
+            {currentTextIndex!==0&&
+        <Button onClick={()=>{setFadeProp({
+                    fade:'fade-out'
+                });setTextIndex(textIndex-1);}}> {"<<<"} </Button>}
+        <h1 className={fadeProp.fade}>
+        {currentText}
+        </h1>
+        <Button onClick={()=>{setFadeProp({
+                    fade:'fade-out'
+                });setTextIndex(textIndex+1);}}>{">>>"}</Button>
+        </div>
+    )
+}
+
+Fader.defaultProps={
+    text: 'Hello there'
+}
+
+Fader.propTypes ={
+    text: PropTypes.string,
+}
+
+export default Fader
