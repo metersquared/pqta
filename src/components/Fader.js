889ab4af58158c55fd89d377eaba2c615f6ff552
Arya Prasetya <arya.prasetya1997@gmail.com>
2022-05-19 21:33:24 +0200

    remove redundant components

--- src/components/Fader.js
+++ src/components/Fader.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+
 
 
 const Fader = ({text,visible, textIndex, setTextIndex}) =>{
